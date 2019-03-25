11cff0cc8d08071416c8e18e7a01d0a787156e65
emranhos1 <emranhos1@DESKTOP-BL5QDMH>
2019-03-25 22:54:45 +0600

    Hospital Info Crud

---
+++ src/test/java/com/eh/hospital/HospitalInfoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eh.hospital;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HospitalInfoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
